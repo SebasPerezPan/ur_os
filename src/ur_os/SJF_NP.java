68e1db6b7295a7edf6a2bad610a5bd348fc33f25
Sebastian Perez <128326496+SebasPerezPan@users.noreply.github.com>
2025-02-25 20:02:48 -0500

    Fix(global): getNext() for SJF NP completed.

--- src/ur_os/SJF_NP.java
+++ src/ur_os/SJF_NP.java
@@ -9,16 +9,39 @@ public class SJF_NP extends Scheduler {
     }
 
     @Override
-    public void getNext(boolean cpuEmpty) {
-        if (!processes.isEmpty() && cpuEmpty) {        
-            // Encontrar el proceso con el menor tiempo total de ejecución
-            Process shortestJob = processes.stream().min(Comparator.comparingInt(Process::getRemainingTimeInCurrentBurst)) // Selecciona el proceso más corto
-                .orElse(null);
+//    public void getNext(boolean cpuEmpty) {
+//        if (!processes.isEmpty() && cpuEmpty) {
+//            // Encontrar el proceso con el menor tiempo total de ejecución
+//            Process shortestJob = processes.stream().min(Comparator.comparingInt(Process::getRemainingTimeInCurrentBurst)) // Selecciona el proceso más corto
+//                .orElse(null);
+//
+//            if (shortestJob != null) {
+//                processes.remove(shortestJob); // Elimina el proceso seleccionado de la lista
+//                os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, shortestJob); // Asigna el proceso a la CPU
+//            }
+//        }
+//    }
 
-            if (shortestJob != null) {
-                processes.remove(shortestJob); // Elimina el proceso seleccionado de la lista
-                os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, shortestJob); // Asigna el proceso a la CPU
+    // Esta es la implementación del pseudo-codigo usando un proceso de iteración entre los remaining, con el metodo que pillamos xD
+    public void getNext(boolean cpuEmpty) {
+        if (!processes.isEmpty() && cpuEmpty){
+            Process min_BTR_process = null;
+            int min_BTR = 9999;
+
+            for (Process p: processes){
+                if (p.isCurrentBurstCPU()){
+                    int temp = p.getRemainingTimeInCurrentBurst();
+                    if(temp < min_BTR) {
+                        min_BTR = temp;
+                        min_BTR_process = p;
+                    } else if (temp == min_BTR) {
+                        min_BTR_process = tieBreaker(min_BTR_process, p);
+
+                    }
+                }
             }
+            os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, min_BTR_process);
+            processes.remove(min_BTR_process);
         }
     }
 
